2011f9ca85ef3039456d9aea18fbceb65ae7e056
mati <fantomx775@gmail.com>
2023-10-27 19:50:52 +0200

    lab4

---
+++ lab4/lab-gui-gallery/src/main/java/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+import javafx.application.Application;
+
+public class Main {
+
+    public static final String SCRAPER_API_KEY = "8c78cfbbfe9fb8d7641112a3a2a5a79e";
+
+
+    public static void main(String[] args) {
+        Application.launch(GalleryApp.class);
+    }
+}
